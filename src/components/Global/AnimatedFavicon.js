8a5ed500d6bf293249810ee4bf6322ddff7271e8
Khandaker Samin Yeasar <138380913+KhandakerSamin@users.noreply.github.com>
2026-03-12 15:37:31 +0600

    again fix the favicon

--- src/components/Global/AnimatedFavicon.js
+++ src/components/Global/AnimatedFavicon.js
@@ -4,19 +4,25 @@ import { useEffect } from "react";
 export default function AnimatedFavicon() {
   useEffect(() => {
     const favicons = ["/favicon1.png", "/favicon2.png"];
-    let index = 1;
+    let index = 0;
+
+    const getLink = () => {
+      let link = document.querySelector("link[rel='icon']");
+      if (!link) {
+        link = document.createElement("link");
+        link.rel = "icon";
+        link.type = "image/png";
+        document.head.appendChild(link);
+      }
+      return link;
+    };
+
+    // Set favicon1 immediately on mount
+    getLink().href = favicons[0];
 
     const interval = setInterval(() => {
       index = index === 0 ? 1 : 0;
-      const link =
-        document.querySelector("link[rel='icon']") ||
-        document.createElement("link");
-      link.rel = "icon";
-      link.type = "image/png";
-      link.href = favicons[index];
-      if (!document.querySelector("link[rel='icon']")) {
-        document.head.appendChild(link);
-      }
+      getLink().href = favicons[index];
     }, 3000);
 
     return () => clearInterval(interval);
