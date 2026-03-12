6420f70d416bc66cbc00b218033c2de961b09ff6
Khandaker Samin Yeasar <138380913+KhandakerSamin@users.noreply.github.com>
2026-03-12 16:15:34 +0600

    added gmail link

--- src/components/AboutPage/Gallery.js
+++ src/components/AboutPage/Gallery.js
@@ -301,13 +301,17 @@ export default function Gallery() {
         {/* CTA Button */}
         <div className="flex items-center justify-center gap-2 mt-14 md:mt-20 group/cta">
           <a
-            href="mailto:hr@oriture.co"
+            href="https://mail.google.com/mail/?view=cm&fs=1&to=hr@oriture.co"
+            target="_blank"
+            rel="noopener noreferrer"
             className="border border-[#D1FF52]/50  text-black bg-[#D1FF52] group-hover/cta:border-[#D1FF52]  px-6 py-3 rounded-full transition-all duration-300 text-sm font-medium whitespace-nowrap"
           >
             Send your CV to Join
           </a>
           <a
-            href="mailto:hr@oriture.co"
+            href="https://mail.google.com/mail/?view=cm&fs=1&to=hr@oriture.co"
+            target="_blank"
+            rel="noopener noreferrer"
             className="border border-[#D1FF52]/50 bg-transparent p-2.5 rounded-tr-full rounded-b-full group-hover/cta:rounded-t-full group-hover/cta:rounded-bl-none group-hover/cta:bg-[#D1FF52] transition-all duration-300 cursor-pointer flex items-center justify-center"
           >
             <ArrowUpRight className="w-4 h-4 text-[#D1FF52] group-hover/cta:rotate-45 group-hover/cta:text-black transition-all duration-300" />
